4bd8eefbde1b687a994d16ae5f837795b4e025d8
luislaurel <luislaurel133@gmail.com>
2022-08-21 20:22:09 -0600

    Completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,4 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
-
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -12,7 +11,12 @@
 
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
-
+// Psuedo code
+// Create a describe code block for test - 
+// Will take in a function - call it fibSequence
+// it - returns an array of that length(?) containing the numbers of Fibonacci sequence
+// expect - [1, 1, 2, 3, 5, 8], - [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// 
 // a) Create a test with expect statements for each of the variables provided.
 
 const fibLength1 = 6
@@ -21,27 +25,139 @@ const fibLength1 = 6
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+    describe("fibSequence", () => {
+        it("returns an array that length containing the numbers of Fibonacci sequence.", () => {
+            expect(fibSequence(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+            expect(fibSequence(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+        })
+    })
 
+
+
+ FAIL  ./code-challenges.test.js
+  ● Test suite failed to run
+
+    ReferenceError: fibSequence is not defined
+
+      27 | // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+      28 |
+    > 29 |     describe(fibSequence, () => {
+         |              ^
+      30 |         it("returns an array that length containing the numbers of Fibonacci sequence.", () => {
+      31 |             expect(fibSequence(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+      32 |             expect(fibSequence(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+
+      at Object.fibSequence (code-challenges.test.js:29:14)
+
+// Test Suites: 1 failed, 1 total
 // b) Create the function that makes the test pass.
 
+// Psuedo code
+// Create my func named fibSequence
+// It will take in fibLength1,2 as a argument 
+// Using Fibonacci Sequence - is a series of numbers starting at 0 and 1. next numb is previous 2 numbers added up
+// Pass value into parameter
+// Array must be size of num in fibLength
+// fibL1 is = 6 so it must contain 6 values
+// Create a new var with empty array or .map() to create new Array?
+// Need to start at 0 and 1.
+//  create an Array of 0,1 for starting points in Fib. 
+// create a loop but I already have 0,1. So start at 2
+//  I want to push the new numbers into my array .push()
+// Forgot I don't want 0 in my array, so I need to .slice() it out
+// add slice to return
+// output
+// [1, 1, 2, 3, 5, 8]
+// [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+const fibSequence =  (num) => {
+let fibArr = [0,1]
+ for (let i = 2; i <= num; i++) {
+      fibArr.push(fibArr[i - 2] + fibArr[i - 1])
+        } return fibArr.slice(1)
+
+
+}
+
+ PASS  ./code-challenges.test.js
+  fibSequence
+    ✓ returns an array that length containing the numbers of Fibonacci sequence. (5 ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       1 passed, 1 total
+Snapshots:   0 total
+Time:        0.569 s, estimated 1 s
+
+// console.log(fibSequence(fibLength1))
+// console.log(fibSequence(fibLength2))
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
-
+// create describe code block to test
+// take in func of onlyOddNum
+// it - takes in an array and returns a new array of only odd numbers sorted from least to greatest.
+// expect - [-9, 7, 9, 199] - [-823, 7, 23]
 const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
 
 const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+describe("onlyOddNum", ()=> {
+    it("takes in an array and returns a new array of only odd numbers sorted from least to greatest.", () => {
+        expect(onlyOddNum(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(onlyOddNum(fullArr2)).toEqual([-823, 7, 23])
+    })
+})
+
+ FAIL  ./code-challenges.test.js
+  onlyOddNum
+    ✕ takes in an array and returns a new array of only odd numbers sorted from least to greatest. (1 ms)
+
+  ● onlyOddNum › takes in an array and returns a new array of only odd numbers sorted from least to greatest.
+
+    ReferenceError: onlyOddNum is not defined
 
 // b) Create the function that makes the test pass.
+
+//Psuedo Code
+// create func named onlyOddNum
+// create a variable to store new arr
+// I only want odd numbers and need to be sorted from least to greatest.
+// .filter through fullArr variables  to find odd numbers -
+// use type of to find all my numbers
+// once i get my nums i need to find odd nums only, so use % 
+// value % 2 !== 0 will find my odds
+// sort them from least to greatest. - use .sort? 
+// used .sort() to get them from least to greatest 
+
+
+const onlyOddNum = (array) => {
+   let getNums = array.filter((value)=> 
+        typeof value  === 'number' && value % 2 !== 0) 
+    return getNums.sort((a,b) => a-b)
+    
+ } 
+// console.log(onlyOddNum(fullArr1))
+// console.log(onlyOddNum(fullArr2))
+
+ PASS  ./code-challenges.test.js
+  onlyOddNum
+    ✓ takes in an array and returns a new array of only odd numbers sorted from least to greatest. (2 ms)
+
+// Had issue with it returning a blank array because I used {} after my arrow func in .filter()
+// good example I just found of an implicit return also ^
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// describe will take in my func named "accSum"
+// it - takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+// expect - [2, 6, 51, 60] - [0, 7, -1, 11] - []
 
 const numbersToAdd1 = [2, 4, 45, 9]
 // Expected output: [2, 6, 51, 60]
@@ -52,5 +168,73 @@ const numbersToAdd2 = [0, 7, -8, 12]
 const numbersToAdd3 = []
 // Expected output: []
 
+describe("accSum", () => {
+    it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+        expect(accSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(accSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(accSum(numbersToAdd3)).toEqual([])
+    })
+})
+
+
+ FAIL  ./code-challenges.test.js
+  accSum
+    ✕ takes in an array and returns an array of the accumulating sum. An empty array should return an empty array. (1 ms)
+
+  ● accSum › takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+    ReferenceError: accSum is not defined
+
+      152 | describe("accSum", () => {
+      153 |     it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+    > 154 |         expect(accSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+          |         ^
+      155 |         expect(accSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+      156 |         expect(accSum(numbersToAdd3)).toEqual([])
+      157 |     })
 
 // b) Create the function that makes the test pass.
+
+//Psuedo code
+// Make a function named accSum
+// input numberstoAdd1,2,3
+// Need to add previous 2 indexes to create next index
+// So 2 + 4 = 6... 6 + 45 = 51
+// create a for loop to iterate through array, needs to start at 2nd index. (1)
+// add current number at that index + previous index 
+// will return the total --> iterates through all nums 
+
+// input 
+// output: [2, 6, 51, 60], [0, 7, -1, 11], []
+
+ // [2, 4, 45, 9]
+
+const accSum = (nums) => {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = nums[i] + nums[i - 1]
+    } return nums 
+
+}
+// so first loop will start at 4, nums[i] = 4. 4 + [nums i = 1] this will take previous index. which is 2. so 4 + 2 = 6.
+// console.log(accSum(numbersToAdd1))
+// console.log(accSum(numbersToAdd2))
+// console.log(accSum(numbersToAdd3))
+
+ PASS  ./code-challenges.test.js
+  accSum
+    ✓ takes in an array and returns an array of the accumulating sum. An empty array should return an empty array. (3 ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       1 passed, 1 total
+Snapshots:   0 total
+Time:        0.431 s, estimated 1 s
+
+
+ // Another way I found to do it, much shorter but don't think i could've came up with this on my own.
+// const accSum = (array) => {
+//     let addNums = array.map((sum = 0, n => sum += n)) 
+//         return addNums 
+// }
+// console.log(accSum(numbersToAdd1))
+// console.log(accSum(numbersToAdd2))
+// console.log(accSum(numbersToAdd3))
--- interview-questions.md
+++ interview-questions.md
@@ -9,32 +9,57 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
 Your answer:
-
+I have worked with JSX in React, it is the combination of JS and HTML inside of React. I know when creating a class inside of JSX we aren't able to use the word class to do so. This is because Javascript already has class as a set keyword, so in order for to by pass we must use className instead. This would like <h1 className="CantUseClass"></h1>. Although I believe I may need to work on proper syntax inside of React. The work I have with JSX so far has been good to work with. 
 Researched answer:
+
+JSX is an extension of JavaScript inside of React. It stands for JavaScript XML. This allows for the use of HTML inside of React, although this is not a requirement for React. So when we use JSX we are creating a element inside of React. You are able to code without JSX. From what I have researched, it is an easier way of creating React applications. I see it is better because you can work HTML and JavaScript at once instead of going to HTML to put our JS. A syntax difference I know is you cannot create a class using this naming convention.(Like class="madeMyClass") In order to create a class you must use className instead. (className="madeMyClass") This is because JavaScript has class as a reserved key word. 
 
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-Your answer:
-
+Your answer: I know yarn is a resource negotiator, it actually stands for Yet Another Resource Negotiator.
+When we run yarn on our terminal I believe it grabs the node dependencies which we need to view our app in the browser. 
 Researched answer:
+I've learned that YARN was developed by FaceBook and several other companies. It works with Node.js. It is how dependencies are managed, dependencies are what projects need into order to work properly. So in my experience I've used it to install node modules. It was also created as better alternative to NPM. Although I don't know about NPM. 
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: I have worked with anonymous functions when it comes to higher order functions. One way I've used it passing it inside the parameters of .map() as an argument. I believe the difference is that these functions have not been declared. When I use an arrow function the way I would create this is have a variable assigned to it. So it would look like const ArrowFunc = () => {}. I believe its usefulness is that we are able to pass it as argument into a parameter. Although I am not completely sure on this but in my usage of it so far, this is what my conclusion would be.
 
 Researched answer:
+  It is a function that has been declared but there is no variable assigned to it. I now know that because it has not been assigned we would not be able to access it after. But the reason I am able to use it after a .map() is because I will call upon the variable I used .map on. So its purposes would be we can pass it as a argument to another function. I have also learned that it is executed right after being declared. 
 
 4. What is the difference between a class and an object?
 
-Your answer:
+Your answer: While I have worked with class and object, I have more a of liking for classes. I will go ahead an explain what an Object is first. An Object data is stored inside of key: value pairs. I like to think of the key as the name then it is followed by the data. An example would be name: "Luis". Although this is not the actual syntax to complete the Object. I know in order to call on this key:pair I need to use dot notation. This would be needed to pass anything into it also. Next I would like to explain about Class. So a Class is essentially the blueprint we use to create our Objects. It contains Objects inside of it. This is where the differences come in. In order to create a Class we require a constructor. This is how a object is made or called on. Next unlike a Object we need to use this. to create our objects and access them. We also have the keyword new if we are creating a new class to give it access to it.
+
+Object example:
+const myObject = {
+{name: Luis} <-- key:value pair
+{name: Bob}
+}
+
+Class example:
+class MyClass{
+    constructor()  > {
+        this.Bird
+        this.Fish
+        this.Snake <-- Use this. to create my obj
+    }
+}
+var NewClass = new MyClass <--- this var can now access MyClass
+
 
 Researched answer:
+So, essentially I know that Classes are the blueprints for Objects. Classes make up the data and behavior for Objects. It is the properties and methods that our Object will end up having. An Object is just an instance of a class, while Classes are just how Objects are made. I've learned an Object is a physical thing vs Class which is more a of logical entity. Which is something I've known all along just didn't look at it that way.
+
 
 5. What is the difference between state and props in React?
 
-Your answer:
+Your answer: I am aware of how State and Prop work from my experience in React. Although I may not be able to go to deeply in the differences. I know a State is what we use to store our initial data which we can then use a setState to change. We must also create an import for useState. A prop is a property is how we pass data down to our components. It is passed down using props{variablename}. In my example I have gave props could be named anything so it is best to keep it named something that relates to what we are doing. In my component in order for me to access that prop you must use props.<whatever I named it> so props.props. This would not be a good naming convention for it though as it can be confusing.
 
 Researched answer:
+ Some of the key differences are props can pass data from one component to another, while State will only pass data in the component only. This is something I should've also realized as we haven't tried to change State in a different component while using React. A better way I learned to understand them is props are immutable while a State is mutable. I also have learned that props have better performance in comparison to State. 
+
 
 6. STRETCH: Which is the difference between a div and a span?
 
@@ -44,12 +69,12 @@ Researched answer:
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: A way of creating new objects from a given object. It's based on Objects and Classes.
 
-2. Ruby:
+2. Ruby: It is a fully OOP language. The general use of it is for creating web apps, although not limited to this.
 
 3. Implicit return:
-
+        It returning a value in an arrow func by getting rid of {}. If you use curly braces you will get undefined, ran into this on my challenges. When using this on Obj you have to wrap it in ().
 4. Ruby blocks:
-
-5. Ruby hashes:
+ They are anonymous functions that can be passed into a method. 
+5. Ruby hashes: A collection of key value pairs. 
